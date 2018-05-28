b9918f9cf7e318c85829008b1d4a9c924d12823f
Derek Howard <derekhoward@gmail.com>
2018-05-27 23:41:06 -0400

    add new feature groups to selections

--- feature_groups.py
+++ feature_groups.py
@@ -5,7 +5,8 @@ import config
 import os
 
 #just read the header
-df = pandas.read_csv(os.path.join(config.DATA_DIR,'interim', 'processed_features_plus_doc2vec_plus_openAI.csv'), low_memory=False, nrows=1)
+#df = pandas.read_csv(os.path.join(config.DATA_DIR,'interim', 'processed_features_plus_doc2vec_plus_openAI.csv'), low_memory=False, nrows=1)
+df = pandas.read_csv(os.path.join(config.DATA_DIR,'interim', 'all_features_slim.csv'), low_memory=False, nrows=1)
 
 #print(str(list(df.columns)))
 
@@ -25,10 +26,23 @@ VADER_COLS = {x for x in list(df.columns) if x.startswith("vader")}
 
 EMPATH_COLS = {x for x in list(df.columns) if x.startswith("empath")}
 
+TF_IDF_2GRAM_COLS = {x for x in list(df.columns) if x.startswith("tf_idf_2gram_")}
+
+TF_IDF_COLS = {x for x in list(df.columns) if x.startswith("tf_idf_")} - TF_IDF_2GRAM_COLS
+
+ALL_TF_IDF_COLS = {x for x in list(df.columns) if x.startswith("tf_idf_")} #bad naming here - done in a rush
+
+LDA_COLS = {x for x in list(df.columns) if x.startswith("LDA")}
+
 BOARD_COLS = {x for x in list(df.columns) if "board_1hot_" in x}
 AUTHOR_RANK_COLS = {x for x in list(df.columns) if "author_rank_1hot_" in x}
 
-OPENAI_SENTIMENT_COLS = {x for x in list(df.columns) if "openAI_feature_" in x}
+OPENAI_SENTIMENT_COLS = {x for x in list(df.columns) if "openAI_" in x}
+
+DEEPMOJI_COLS = {x for x in list(df.columns) if "deepmoji_" in x}
+#WHOLE_DEEPMOJI_COLS = {x for x in list(df.columns) if ("deepmoji_" "sent") in x}
+
+UNIVERSAL_COLS = {x for x in list(df.columns) if "universal_" in x}
 
 #Free text columns
 NON_NUMERIC_COLS = {'subject', 'cleaned_body', 'images', 'board', 'author_rank', 'post_time'}
@@ -41,9 +55,10 @@ ID_COLS = { 'post_id', 'index', 'author_id', 'board_id', 'edit_author_id', 'pare
 #views, kudos
 NOT_AVAIL_WHEN_FIRST_POSTED = {'kudos_count', 'last_edit_time', 'views', 'following_post_id', 'following_staff_post_id'}
 
-ALL_WHEN_POSTED = set(df.columns) - NOT_AVAIL_WHEN_FIRST_POSTED - ID_COLS - LABEL_COLS - NON_NUMERIC_COLS - OPENAI_SENTIMENT_COLS
+#ALL_WHEN_POSTED = set(df.columns) - NOT_AVAIL_WHEN_FIRST_POSTED - ID_COLS - LABEL_COLS - NON_NUMERIC_COLS - OPENAI_SENTIMENT_COLS
+ALL_WHEN_POSTED = set(df.columns) - NOT_AVAIL_WHEN_FIRST_POSTED - ID_COLS - LABEL_COLS - NON_NUMERIC_COLS - DEEPMOJI_COLS - OPENAI_SENTIMENT_COLS - UNIVERSAL_COLS
 
-ALL_WHEN_POSTED_INCL_OPENAI = ALL_WHEN_POSTED | OPENAI_SENTIMENT_COLS
+#ALL_WHEN_POSTED_INCL_OPENAI = ALL_WHEN_POSTED | OPENAI_SENTIMENT_COLS
 
 #columns we will have at test time - everything else?
 print("feature_groups.py set: " + str([x for x in locals().keys() if x.upper() == x]))
